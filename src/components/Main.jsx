c9c510f29ad0d65e43a4eb08d44444a430e11a16
IgorGoro91 <igorysc@gmail.com>
2025-02-18 19:15:32 +0100

    add:react

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+const Main = () =>{
+    return(
+
+        <main>
+            <section className="titolo">
+                <h1> Content goes here </h1>
+            </section>
+
+            <section className="market">
+
+            </section>
+        </main>
+    )
+}
+
+export default Main
